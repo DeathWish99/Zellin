b189cf3c27b47be7a92570371cb671a9f2b972a6
DeathWish99 <kevintrisnadi@gmail.com>
2018-10-27 11:10:26 +0700

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform detectPlayer;
+    public int health;
+    public float speed;
+    public float detectRangeX;
+    public float detectRangeY;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D[] playerDetect = Physics2D.OverlapBoxAll(detectPlayer.position, new Vector2(detectRangeX, detectRangeY), 0);
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Took Damage");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(detectPlayer.position, new Vector3(detectRangeX, detectRangeY, 1));
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float jumpForce = 1.0f;
+    public float doubleJumpCooldown = 0.1f;
+    private float doubleJumpCounter = 0.0f;
+    private bool isOnGround = true;
+    private bool isDoubleJumping = false;
+    //private Animator anim;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(translation, 0, 0);
+
+        if (translation < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        //if (translation != 0)
+        //{
+        //    anim.SetFloat("Player Speed", speed);
+        //}
+        //else
+        //{
+        //    anim.SetFloat("Player Speed", 0);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isOnGround)
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                isOnGround = false;
+            }
+            else
+            {
+                if (isDoubleJumping == false)
+                {
+                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                    isDoubleJumping = true;
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isOnGround = true;
+            isDoubleJumping = false;
+        }
+    }
+}
