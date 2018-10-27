6e11fb123d783e72d4afc1d5b678aa45ff026b25
Minhuei <chang.minhuei@gmail.com>
2018-10-27 11:48:21 +0700

    Update

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+    int index;
+	
+    public void ChangeScene(int index) { 
+           SceneManager.LoadScene(index);
+    }
+
+
+}
