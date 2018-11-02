8d37c3341f3765974113ac9d9ad1bdf5fe16a5a5
Minhuei <chang.minhuei@gmail.com>
2018-11-02 14:30:57 +0700

    Update

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+
+    IEnumerator Splash_Screen()
+    {
+        yield return new WaitForSeconds(2);
+        Application.LoadLevel(1);
+    }
+
+    void Start () {
+        StartCoroutine(Splash_Screen());
+	}
+    
+   
+	
+}
---
+++ Assets/Scripts/SplashScreenUnity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenUnity : MonoBehaviour {
+
+
+    IEnumerator SplasshScreenUnity()
+    {
+        yield return new WaitForSeconds(2);
+        Application.LoadLevel(2);
+    }
+
+    void Start () {
+        StartCoroutine(SplasshScreenUnity());
+	}
+    
+   
+	
+}
