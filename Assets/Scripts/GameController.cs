cf367a6fa0c1991dd05616dff9b8973b9d3470d0
hansbudiman <hansbudiman62@gmail.com>
2018-11-03 12:36:30 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/DeathWish99/Zellin""
    This reverts commit 515238dc227967b84f02c0f08aebda412b4d9771.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,14 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
     public Text scoreText;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+    public Text healthText;
+    public int playerHealth = 5;
+=======
+>>>>>>> 6fd58527ca60b3d8e376506fb461fecb2c2dd0f1
+>>>>>>> parent of 515238d... Revert "Merge branch 'master' of https://github.com/DeathWish99/Zellin"
 
     private int score;
 
@@ -13,6 +21,13 @@ public class GameController : MonoBehaviour {
 	void Start () {
         score = 0;
         UpdateScore();
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+        UpdateHealth();
+=======
+>>>>>>> 6fd58527ca60b3d8e376506fb461fecb2c2dd0f1
+>>>>>>> parent of 515238d... Revert "Merge branch 'master' of https://github.com/DeathWish99/Zellin"
     }
 
     // Update is called once per frame
@@ -21,7 +36,30 @@ public class GameController : MonoBehaviour {
         UpdateScore();
     }
 
+<<<<<<< HEAD
     void UpdateScore () {
         scoreText.text = "Score: " + score;
 	}
+=======
+<<<<<<< HEAD
+    public void TakeDamage(int damageValue)
+    {
+        playerHealth -= damageValue;
+        UpdateHealth();
+    }
+
+    void UpdateScore () {
+        scoreText.text = "Score: " + score;
+	}
+
+    void UpdateHealth ()
+    {
+        healthText.text = "Health: " + playerHealth;
+    }
+=======
+    void UpdateScore () {
+        scoreText.text = "Score: " + score;
+	}
+>>>>>>> 6fd58527ca60b3d8e376506fb461fecb2c2dd0f1
+>>>>>>> parent of 515238d... Revert "Merge branch 'master' of https://github.com/DeathWish99/Zellin"
 }
