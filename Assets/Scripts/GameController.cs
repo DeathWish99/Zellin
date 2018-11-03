ef5253290ac218001e8dbc172e1d4cecc48cfad1
DeathWish99 <DeathWish99@users.noreply.github.com>
2018-11-03 10:21:39 +0700

    Update

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text scoreText;
+    public Text healthText;
+    public int playerHealth = 5;
+
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        UpdateScore();
+        UpdateHealth();
+    }
+
+    // Update is called once per frame
+    public void AddScore(int newScoreValue) {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    public void TakeDamage(int damageValue)
+    {
+        playerHealth -= damageValue;
+        UpdateHealth();
+    }
+
+    void UpdateScore () {
+        scoreText.text = "Score: " + score;
+	}
+
+    void UpdateHealth ()
+    {
+        healthText.text = "Health: " + playerHealth;
+    }
+}
