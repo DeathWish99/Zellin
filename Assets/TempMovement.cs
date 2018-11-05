6ba428ebf2dcf7bd8089442894236c5b56d64391
DeathWish99 <DeathWish99@users.noreply.github.com>
2018-11-05 20:31:36 +0700

    Not my Problem

---
+++ Assets/TempMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMovement : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 translation = new Vector3(speed * Time.deltaTime, 0, 0);
+
+		if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(translation);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(translation * -1);
+        }
+	}
+}
